8df7306ece65fea4a116a5471c58ab37af8e0b39
Farhan Ali <farhanyounas5204@gmail.com>
2023-09-03 22:06:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+// var userNames:string[]=["Leo","Farhan","Ali","Asad"];
+// // for(let index=0;index<userNames.length;index++){
+// //     let userName=userNames[index];
+// //     console.log("username",userName);
+// // }
+// // function getValue(userName:string,index:number, fullArray:string[]){
+// // }
+// userNames.forEach((value:string,index:number, fullArray:string[])=>{
+//     console.log("userName",userNames)
+//     console.log("index",index)
+//     console.log("userName",fullArray)
+// })
+// for each is not returned anything, 
+// write a ts program that reshap the input array [2,5,10,20] to multiply by 2 and print the new list
+// objcets
+let student = {
+    Name: "Asad",
+    rollNo: 3002,
+};
+console.log(student);
+let names = ["Farhan", "Talha", "Abdulrehman", "javeria", "Asad"];
+let student1 = ["Farhan", 2020, "Evening"];
+let student2 = ["Asad", 2020, "Evening"];
+let student3 = ["Asad", "Evening", 2020];
+student1[2];
+student1[2];
+student1[2];
