4b6b9edfcde9e3ca901a2e0f8932cb91557bbdef
Farhan Ali <farhanyounas5204@gmail.com>
2023-09-11 20:40:41 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 "use strict";
-// var userNames:string[]=["Leo","Farhan","Ali","Asad"];
-// // for(let index=0;index<userNames.length;index++){
-// //     let userName=userNames[index];
-// //     console.log("username",userName);
-// // }
-// // function getValue(userName:string,index:number, fullArray:string[]){
-// // }
-// userNames.forEach((value:string,index:number, fullArray:string[])=>{
-//     console.log("userName",userNames)
-//     console.log("index",index)
-//     console.log("userName",fullArray)
+// sync 
+// async typescript
+// console.log("start "); // line 1
+// setTimeout(()=>{
+//     console.log("insettimeout");
+// },5000); // it will show output after 5 second
+// setInterval(()=>{
+//     console.log("setIntervel");
+// },1000) // it will print the statement after every 1 second
+// console.log("end ");
+// function printInfunction(){
+//     const time=new Date();
+//     const _time= time.toLocaleDateString();
+//     console.log("leo");
+// }
+// let count =0;
+// let interval = setInterval(()=>{
+//     printInfunction();
+//     count++;
 // })
-// for each is not returned anything, 
-// write a ts program that reshap the input array [2,5,10,20] to multiply by 2 and print the new list
-// objcets
-let student = {
-    Name: "Asad",
-    rollNo: 3002,
+let car = {
+    model: "civic",
+    brand: "honda",
 };
-console.log(student);
-let names = ["Farhan", "Talha", "Abdulrehman", "javeria", "Asad"];
-let student1 = ["Farhan", 2020, "Evening"];
-let student2 = ["Asad", 2020, "Evening"];
-let student3 = ["Asad", "Evening", 2020];
-student1[2];
-student1[2];
-student1[2];
+class Car {
+    constructor() {
+        this.model = "";
+        this.brand = "";
+    }
+}
+let newCar = new Car();
+console.log(newCar);
+car.brand = "bmw";
+car.model = "2020";
+console.log(newCar);
