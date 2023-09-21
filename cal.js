6526f8df822a47da6eb63398e37bc9fe51db7616
Farhan Ali <farhanyounas5204@gmail.com>
2023-09-21 22:57:49 +0500

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+var userInput = inquirer.prompt([
+    {
+        type: "input",
+        name: "firstNum",
+        message: "Enter your First Number",
+        validate: (ans) => {
+            if (!ans) {
+                return "Enter your Number please";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "secondNum",
+        message: "Enter Your Second Number",
+        validate: (ans) => {
+            if (!ans) {
+                return "Enter your Number please";
+            }
+            return true;
+        }
+    },
+    {
+        type: "list",
+        name: "operations",
+        message: "Select the operation you want to perform",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulos", "Exponent"],
+        validate: (ans) => {
+            if (!ans) {
+                return "Enter your Number please";
+            }
+            return true;
+        }
+    }
+]);
