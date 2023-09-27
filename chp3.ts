91e27f882b3695e9e4a0c3e1cf833eefa972c62f
Farhan Ali <farhanyounas5204@gmail.com>
2023-09-27 22:39:16 +0500

    Add files via upload

---
+++ chp3.ts
@@ -0,0 +1,19 @@
+var a:number=7;
+console.log(a);
+var b;
+b=8 // number
+
+b="k"// string
+console.log(typeof b);
+
+
+// union <<<< 
+let Age:number | string = "eighteen";
+
+let age:string | null=null;
+if(1>0.5){
+    age="okiess"
+}
+
+console.log(typeof age);
+
