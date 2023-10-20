9a10c5cd02091db8de11b78cde8a507dc7b0ca33
Farhan Ali <farhanyounas5204@gmail.com>
2023-10-20 21:57:44 +0500

    Add files via upload

--- fun.ts
+++ fun.ts
@@ -22,3 +22,43 @@ function song(Song: string, singer?: string) {
 
 }
 song("leo","Farhan")
+
+
+
+
+
+function add1(num1: number = 10, num2: number = 10): number {
+
+    let result: number = num1 + num2;
+    return result;
+
+}
+
+const values1: number = add();
+console.log(values1);
+
+
+function song1(Song: string, singer?: string) {
+    console.log(Song);
+
+    if (singer) {
+        console.log("heheherhehhhhhhhe");
+
+    }
+
+
+}
+song1("leo","Farhan")
+
+
+function Add(num:number, num1:number) {
+    return num + num1;
+}
+
+Add(10,20)
+
+function Add1(num:number, num1:number) {
+    return num - num1;
+}
+
+Add1(10,20)
